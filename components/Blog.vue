242e07b69eb0da6a2e91880f8bc43ef2cf248a43
Abdel <medjaherirany@gmail.com>
2020-04-25 03:54:01 +0200

    add icons and style

--- components/Blog.vue
+++ components/Blog.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container w-auto mx-auto p-4 md:p-16">
-    <h1 class="font-bold text-xl mb-3">Blog</h1>
+  <div class="container w-auto mx-auto px-6 sm:px-16 mt-8">
+    <h1 class="icon font-bold text-3xl">Blog</h1>
     <ul class="sm:grid sm:grid-cols-2 gap-10">
-      <li v-for="blogPost in blogPosts" :key="blogPost" class="mb-10">
+      <li v-for="blogPost in blogPosts" :key="blogPost" class="mt-10">
         <img :src="blogPost.img" alt="article image" />
-        <h1 class="text-xl font-bold mt-1 mb-1">{{ blogPost.title }}</h1>
-        <p class="text-gray-700 text-sm">
+        <h1 class="text-2xl font-bold mt-1 mb-1">{{ blogPost.title }}</h1>
+        <p class="text-gray-700 sm:text-xl">
           {{ blogPost.description }}
         </p>
       </li>
@@ -30,5 +30,18 @@ h1 {
 
 p {
   font-family: 'Dosis', sans-serif;
+}
+
+.icon {
+  position: relative;
+}
+.icon:after {
+  content: '';
+  position: absolute;
+  height: 38px;
+  width: 38px;
+  top: 10%;
+  margin-left: 16px;
+  background-image: url('~assets/img/iconblog3.svg');
 }
 </style>
--- components/Hero.vue
+++ components/Hero.vue
@@ -10,15 +10,14 @@
         <div
           class="hero-text bg-gray-200 flex items-center p-6 text-justify mx-auto h-20 w-72 shadow-md"
         >
-          <h3 class="flex font-body self-center text-sm font-medium">
+          <h3 class="icon flex font-body self-center text-sm font-medium">
             Hi! I'm Abdelrhany Medjaheri,<br />
-            Junior Front-end Developer
+            Junior Front-end Developer<a
+              href="https://drive.google.com/file/d/1QYC_viSKucLjsbD4aymXijeR8ybp6aZK/view?usp=sharing"
+              target="_blank"
+              ><span class="iconhero"></span
+            ></a>
           </h3>
-          <img
-            class="flex-1 h-12 mb-2 ml-3"
-            src="~/assets/img/coffeedev.svg"
-            alt=""
-          />
         </div>
       </div>
     </div>
@@ -28,4 +27,18 @@
 <script>
 export default {}
 </script>
-<style scoped></style>
+<style scoped>
+.icon {
+  position: relative;
+}
+.iconhero {
+  content: '';
+  position: absolute;
+  height: 38px;
+  width: 38px;
+  left: 110%;
+  top: 4%;
+  background-image: url('~assets/img/iconherocv.svg');
+  background-repeat: no-repeat;
+}
+</style>
