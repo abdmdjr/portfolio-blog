626b39d647b9f68cd2ddfe3191d0e6d596c27a3d
Abdel <medjaherirany@gmail.com>
2020-04-29 01:02:15 +0200

    bugs fixed - add routing for articles

--- components/Blog.vue
+++ components/Blog.vue
@@ -4,14 +4,16 @@
       Blog<span class="iconblog w-6 h-6 md:w-8 md:h-8 top-22 ml-3 "></span>
     </h1>
     <ul class="sm:grid sm:grid-cols-2 gap-10">
-      <li v-for="blogPost in blogPosts" :key="blogPost" class="mt-8">
-        <img :src="blogPost.img" alt="article image" />
-        <h1 class="font-title font-bold text-2xl mt-1 mb-1">
-          {{ blogPost.title }}
-        </h1>
-        <p class="font-dosis text-gray-700 sm:text-xl">
-          {{ blogPost.description }}
-        </p>
+      <li v-for="blogPost in blogPosts" :key="blogPost.slug" class="mt-8">
+        <nuxt-link :to="'/' + blogPost.slug">
+          <img :src="blogPost.img" alt="article image" />
+          <h1 class="font-title font-bold text-2xl mt-1 mb-1">
+            {{ blogPost.title }}
+          </h1>
+          <p class="font-dosis text-gray-700 sm:text-xl">
+            {{ blogPost.description }}
+          </p>
+        </nuxt-link>
       </li>
     </ul>
   </div>
