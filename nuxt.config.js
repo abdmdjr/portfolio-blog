753668ef598818895fe8ca66829c299b117c4c9d
Abdel <medjaherirany@gmail.com>
2020-05-08 20:30:26 +0200

    test google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,15 +43,22 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{ src: '@/plugins/aos', ssr: false }],
+  plugins: [
+    { src: '@/plugins/aos', ssr: false },
+    { src: '~plugins/ga.js', mode: 'client' }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
   ],
+  googleAnalytics: {
+    id: 'UA-165934952-1'
+  },
   /*
    ** Nuxt.js modules
    */
